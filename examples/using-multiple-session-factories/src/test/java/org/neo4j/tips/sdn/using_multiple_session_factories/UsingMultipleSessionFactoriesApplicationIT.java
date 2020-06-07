1a797501b87396c8c305020064070b640b245475
Christian Gleissner <a-cgleissner@expedia.com>
2020-06-07 19:23:49 +0200

    Upgraded to latest Neo4J. Start and stop Docker containers automatically.

--- examples/using-multiple-session-factories/src/test/java/org/neo4j/tips/sdn/using_multiple_session_factories/UsingMultipleSessionFactoriesApplicationIT.java
+++ examples/using-multiple-session-factories/src/test/java/org/neo4j/tips/sdn/using_multiple_session_factories/UsingMultipleSessionFactoriesApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UsingMultipleSessionFactoriesApplicationTests {
+public class UsingMultipleSessionFactoriesApplicationIT {
 
   @Test
   public void contextLoads() {
