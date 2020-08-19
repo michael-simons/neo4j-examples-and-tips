05e22925516830c55d4e93e9912c6a7f0498bb16
Michael Simons <michael@simons.ac>
2020-08-19 17:13:40 +0200

    Add article.

--- examples/retries-through-space-and-time/sdn_ogm/src/main/java/org/neo4j/tips/cluster/sdn_ogm/Application.java
+++ examples/retries-through-space-and-time/sdn_ogm/src/main/java/org/neo4j/tips/cluster/sdn_ogm/Application.java
@@ -1,13 +1,23 @@
 package org.neo4j.tips.cluster.sdn_ogm;
 
+import io.github.resilience4j.retry.RetryRegistry;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.core.log.LogMessage;
 
 @SpringBootApplication
 public class Application {
 
+	private static final Log log = LogFactory.getLog(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	public Application(RetryRegistry retryRegistry) {
+		retryRegistry.retry("neo4j").getEventPublisher().onRetry(e -> log.warn(LogMessage.format("Retrying %s", e)));
 	}
 }
