75cd20342b0493a8536a6b847012a7b791193158
Michael Simons <michael@simons.ac>
2021-09-29 16:05:48 +0200

    Polishing.

--- examples/sdn6-multidb-multi-connections/src/main/java/org/neo4j/tips/sdn/sdn6multidbmulticonnections/Neo4jPropertiesConfig.java
+++ examples/sdn6-multidb-multi-connections/src/main/java/org/neo4j/tips/sdn/sdn6multidbmulticonnections/Neo4jPropertiesConfig.java
@@ -1,23 +1,11 @@
 package org.neo4j.tips.sdn.sdn6multidbmulticonnections;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataProperties;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
-import org.springframework.data.annotation.Persistent;
-import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.data.neo4j.core.schema.RelationshipProperties;
-import org.springframework.util.ClassUtils;
-import org.springframework.util.StringUtils;
 
 @Configuration(proxyBeanMethods = false)
 public class Neo4jPropertiesConfig {
